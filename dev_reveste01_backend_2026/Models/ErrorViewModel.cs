50c45b1d45523818f43409e566a1eadb86ebb1ab
mona <valentewilliams@icloud.com>
2026-04-27 17:47:23 +0100

    Add project files.

---
+++ dev_reveste01_backend_2026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dev_reveste01_backend_2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
